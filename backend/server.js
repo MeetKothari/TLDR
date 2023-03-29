a8ee7973952df5dbcf51d691e0ea75b88e397ac4
Raul Vargas <rvargas321@gmail.com>
2023-03-29 18:58:44 -0400

    added routes, models, and preliminary schema

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const mongoose = require('mongoose')
+const usersRoutes = require('./routes/users')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/users', usersRoutes)
 
-// listen for requests, process.env.PORT is a way of referencing our port number which is sensitive information when we push this to the cloud
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000!!!', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+       // listen for requests, process.env.PORT is a way of referencing our port number which is sensitive information when we push this to the cloud
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening port', process.env.PORT)
+        }) 
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
