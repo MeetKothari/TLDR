9d0ddd2546a9dbc3cb88583bf63651f8f9956357
MeetKo <55168454+MeetKo@users.noreply.github.com>
2023-03-29 15:14:27 -0400

    new pages

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,90 +1,92 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactSVG } from 'react-svg';
 
-import React, {useState} from "react";
-import {Link } from "react-router-dom";
+import "../App.css";
+import mySVG from '../components/coffee.svg';
 
-import '../App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+export default function AuthForm(props) {
+  const [authMode, setAuthMode] = useState("signin");
 
-export default function (props) {
-
-    let [authMode, setAuthMode] = useState("signin")
-    const changeAuthMode = () => {
-    setAuthMode(authMode === "signin" ? "signup" : "signin")
-  }
-
-    if (authMode === "signin") {
-    return (
-      <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign In</h3>
-            <div className="text-center">
-              Not registered yet?{" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-                Sign Up
-              </span>
-            </div>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder="Enter email"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <Link to="/home"><button className="btn btn-primary"> Login </button></Link>
-            </div>
-            <p className="text-center mt-2">
-              Forgot <Link to="/forgot-password">password?</Link>
-              </p>
-          </div>
-        </form>
-        <div id="cursor" className="Cursor"></div>
-      </div>
-    )
-  }
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin");
+  };
 
   return (
-    <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign Up!</h3>
-          <div className="form-group mt-3">
-            <label>Email address</label>
+    <div className="Auth-container">
+     <div className="wrapper">
+      <div className="Auth-intro">
+        <h1 className="Auth-intro-words"> Traffic. Weather. Sports. </h1>
+        <h1 className="Auth-intro-words"> Your morning- simplified. </h1>
+        </div>
+        <img src={mySVG} className="coffee" alt="My SVG" />
+        </div>
+      <div className="Auth-content">
+        <h1 className="Auth-title">Welcome to TL;DR. </h1>
+        <h2 className="Auth-subtitle">Please sign in or create an account.</h2>
+        <form className="Auth-form">
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
             <input
               type="email"
-              className="form-control mt-1"
+              className="form-control"
+              id="email"
               placeholder="Enter email"
             />
           </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
             <input
               type="password"
-              className="form-control mt-1"
+              className="form-control"
+              id="password"
               placeholder="Enter password"
             />
           </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
+          {authMode === "signup" && (
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="confirmPassword"
+                placeholder="Confirm password"
+              />
+            </div>
+          )}
+          <button type="submit" className="Auth-button">
+            {authMode === "signin" ? "Sign In" : "Sign Up"}
+          </button>
+          <div className="Auth-switch">
+            {authMode === "signin" ? (
+              <>
+                <span>Don't have an account yet?</span>
+                <button
+                  type="button"
+                  className="Auth-switch-button"
+                  onClick={changeAuthMode}
+                >
+                  Sign Up
+                </button>
+              </>
+            ) : (
+              <>
+                <span>Already have an account?</span>
+                <button
+                  type="button"
+                  className="Auth-switch-button"
+                  onClick={changeAuthMode}
+                >
+                  Sign In
+                </button>
+              </>
+            )}
           </div>
-          <p className="text-center mt-2">
-            Forgot <Link to="/forgot-password">password?</Link>
-            </p>
-        </div>
-      </form>
-      <div id="cursor" className="Cursor"></div>
+          <p className="Auth-forgot-password">
+            Forgot password? <Link to="/forgot-password">Reset here</Link>
+          </p>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
