136c2e9c45a14a5af2a4a969a153453dd7accf7b
Arth <Arthpatel972@gmail.com>
2023-04-24 10:18:30 -0400

    add new todo

--- src/widgets/todo/todo.js
+++ src/widgets/todo/todo.js
@@ -21,8 +21,23 @@ const TodoItem = ({ item, onCheck }) => {
   );
 };
 
-const TodoList = ({ todos, onCheck }) => {
+const TodoList = ({ todos, onCheck, onAdd }) => {
   const [isDragging, setIsDragging] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      onAdd(newTodo);
+      setNewTodo("");
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && newTodo.trim() !== "") {
+      onAdd(newTodo);
+      setNewTodo("");
+    }
+  };
 
   const onStart = () => {
     setIsDragging(true);
@@ -34,13 +49,21 @@ const TodoList = ({ todos, onCheck }) => {
 
   return (
     <Draggable handle={isDragging ? null : ".todo-list"} onStart={onStart} onStop={onStop} defaultPosition={{x: -1050, y: 500}} bounds={{ top: 200, left: -1100, right: -50, bottom: 1000 }} >
-      
-      
       <div className={`todo-list ${isDragging ? "dragging" : ""}`}>
-      <h1>Todo List</h1>
+        <h1>Todo List</h1>
         {todos.map((todo) => (
           <TodoItem key={todo.id} item={todo} onCheck={onCheck} />
         ))}
+        <div className="add-todo">
+          <input
+            style={{ border: "none",outline:"none", borderBottom: "2px solid black" }}
+            placeholder="Add new todo"
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
       </div>
     </Draggable>
   );
@@ -64,9 +87,10 @@ export default function App() {
     );
   };
 
-  return (
-    
-      <TodoList todos={todos} onCheck={handleCheck} />
-    
-  );
+  const handleAddTodo = (newTitle) => {
+    const newTodo = { id: todos.length + 1, title: newTitle };
+    setTodos([...todos, newTodo]);
+  };
+
+  return <TodoList todos={todos} onCheck={handleCheck} onAdd={handleAddTodo} />;
 }
